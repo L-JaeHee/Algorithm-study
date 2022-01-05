3837c2a781b887c700eed8781b558ffbfdcb8870
L-JaeHee <97137391+L-JaeHee@users.noreply.github.com>
2022-01-06 01:06:43 +0900

    [BOJ]/윤년(2753)/브론즈4/코드 개선

--- BAEKJOON/[BOJ]2753.py
+++ BAEKJOON/[BOJ]2753.py
@@ -1,8 +1,6 @@
 year = int(input())
-result = 0
 
-if year % 4 == 0:
-    if year % 100 != 0 or year % 400 == 0:
-        result = 1
-
-print(result)
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    print(1)
+else:
+    print(0)

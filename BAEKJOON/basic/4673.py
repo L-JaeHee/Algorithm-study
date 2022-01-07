e93362b74837ff68e1811851980e05072165e7c0
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2022-01-07 23:35:47 +0900

    21.01.07/코드 개선

--- BAEKJOON/basic/4673.py
+++ BAEKJOON/basic/4673.py
@@ -1,6 +1,9 @@
 # 4673: 셀프 넘버/실버 5
+# index를 활용하지 않는 경우 list보다 set이 유리
 
-result = [i for i in range(1, 10000)]
+# list보다 set이 시간에서 유리 
+result = set(range(1, 10000))
+not_self = set()
 
 def d(num):
     for i in str(num):
@@ -8,8 +11,10 @@ def d(num):
     return num
 
 for i in range(1, 10000):
-    if d(i) in result:
-        result.remove(d(i))
+    not_self.add(d(i))
+
+# set은 unordered이므로 순서대로 출력하기 위해 정렬
+result = sorted(result - not_self)
 
 for i in result:
     print(i)

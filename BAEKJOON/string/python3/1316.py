9b8704039e622ec80e0d6fe360e7b4f42401deb3
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2022-01-11 19:10:54 +0900

    22.01.11/코드 개선

--- BAEKJOON/string/python3/1316.py
+++ BAEKJOON/string/python3/1316.py
@@ -1,9 +1,16 @@
 # 1316: 그룹 단어 체커/실버 5
-
-N = int(input())
+# retry
+"""
+4
+aba
+abab
+abcabc
+a
+-> 1
+"""
 
 def checker(text):
-    words = dict()
+    words = dict() #aba
     for idx in range(len(text)):
         word = text[idx]
         if word not in words:
@@ -15,8 +22,9 @@ def checker(text):
                 words[word] = idx
     return 1
 
+N = int(input())
 result = 0
-for _ in range(N):
+for i in range(N):
     text = input()
     result += checker(text)
 

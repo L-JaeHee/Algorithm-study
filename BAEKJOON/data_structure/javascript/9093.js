7f76ec9ba70573bcd8d960a92f48b1b180ac0f91
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2023-02-17 19:59:02 +0900

    23.02.17 / 복습 및 두 가지 코드 스타일

--- BAEKJOON/data_structure/javascript/9093.js
+++ BAEKJOON/data_structure/javascript/9093.js
@@ -1,10 +1,37 @@
-/*
-  9093: 단어 뒤집기/브론즈 1
-*/
-const fs = require('fs');
-const localPath = __dirname + '/input.txt';  // 로컬 실행용 경로
-const baekPath = '/dev/stdin';  // 백준 제출용 경로
-let input = fs.readFileSync(localPath).toString().trim().split('\n');
+const fs = require("fs");
+let input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : fs
+        .readFileSync(__dirname + "/input.txt")
+        .toString()
+        .trim();
 
+input = input.split("\n");
 input.shift();
-console.log(input.map((string) => string.split(' ').map((word) => word.split('').reverse().join('')).join(' ')).join('\n'));
+
+console.log(
+  input
+    .map((strings) => {
+      return strings
+        .split(" ")
+        .map((words) => {
+          return words.split("").reverse().join("");
+        })
+        .join(" ");
+    })
+    .join("\n")
+);
+
+// const result = [];
+
+// for (let string of input) {
+//   const words = string.split(" ");
+//   const reversedWords = words.map((word) => {
+//     return word.split("").reverse().join("");
+//   });
+
+//   result.push(reversedWords.join(" "));
+// }
+
+// console.log(result.join("\n"));

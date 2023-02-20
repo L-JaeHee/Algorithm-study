6777bd21d48c785d1753563a1aae87d08fbc2410
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2023-02-20 20:52:11 +0900

    23.02.20 / 복습

--- BAEKJOON/data_structure/javascript/10845.js
+++ BAEKJOON/data_structure/javascript/10845.js
@@ -1,60 +1,98 @@
-/*
-  10845: 큐/실버 4
-*/
-const fs = require('fs');
-const localPath = __dirname + '/input.txt';  // 로컬 실행용 경로
-const baekPath = '/dev/stdin';  // 백준 제출용 경로
-let input = fs.readFileSync(localPath).toString().trim().split('\n');
-input.shift();
+const fs = require("fs");
+let input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : fs
+        .readFileSync(__dirname + "/input.txt")
+        .toString()
+        .trim();
 
-const commands = {
-  push: function(que, x) {
-    que.push(x);
-  },
-  pop: function(que) {
-    if (que.length === 0) {
+input = input.split("\n");
+input.shift();
+input = input.map((command) => command.split(" "));
+
+class Queue {
+  constructor() {
+    this.current = [];
+  }
+
+  push(value) {
+    this.current.push(value);
+    return null;
+  }
+
+  pop() {
+    if (this.current.length === 0) {
       return -1;
-    } else {
-      return que.shift();
     }
-  },
-  size: function(que) {
-    return que.length;
-  },
-  empty: function(que) {
-    if (que.length === 0) {
+
+    return this.current.shift();
+  }
+
+  size() {
+    return this.current.length;
+  }
+
+  empty() {
+    if (this.current.length === 0) {
       return 1;
-    } else {
-      return 0;
     }
-  },
-  front: function(que) {
-    if (que.length === 0) {
+
+    return 0;
+  }
+
+  front() {
+    if (this.current.length === 0) {
       return -1;
-    } else {
-      return que[0];
     }
-  },
-  back: function(que) {
-    if (que.length === 0) {
+
+    return this.current[0];
+  }
+
+  back() {
+    if (this.current.length === 0) {
       return -1;
-    } else {
-      return que[que.length - 1];
     }
+
+    return this.current[this.current.length - 1];
   }
 }
 
-const que = [];
-const result = [];
+function solution(input) {
+  const queue = new Queue();
+  const result = [];
 
-for (const value of input) {
-  const command = value.split(' ');
-  if (command[0] === 'push') commands.push(que, command[1]);
-  else if (command[0] === 'pop') result.push(commands.pop(que));
-  else if (command[0] === 'size') result.push(commands.size(que));
-  else if (command[0] === 'empty') result.push(commands.empty(que));
-  else if (command[0] === 'front') result.push(commands.front(que));
-  else if (command[0] === 'back') result.push(commands.back(que));
+  for (let command of input) {
+    const [order, value] = command;
+
+    switch (order) {
+      case "push":
+        queue.push(value);
+        break;
+
+      case "pop":
+        result.push(queue.pop());
+        break;
+
+      case "size":
+        result.push(queue.size());
+        break;
+
+      case "empty":
+        result.push(queue.empty());
+        break;
+
+      case "front":
+        result.push(queue.front());
+        break;
+
+      case "back":
+        result.push(queue.back());
+        break;
+    }
+  }
+
+  return result.join("\n");
 }
 
-console.log(result.join('\n'));
+console.log(solution(input));

843a1875b8d0c650445db47dbe50d2ec7fad7942
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2023-02-20 20:36:59 +0900

    23.02.20 / 복습

--- BAEKJOON/data_structure/javascript/1406.js
+++ BAEKJOON/data_structure/javascript/1406.js
@@ -1,27 +1,40 @@
-/*
-  1406: 에디터/실버 3
-*/
-const fs = require('fs');
-const localPath = __dirname + '/input.txt';  // 로컬 실행용 경로
-const baekPath = '/dev/stdin';  // 백준 제출용 경로
-let input = fs.readFileSync(localPath).toString().trim().split('\n');
+const fs = require("fs");
+let input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : fs
+        .readFileSync(__dirname + "/input.txt")
+        .toString()
+        .trim();
 
-const leftStack = input.shift().split('');
-const rightStack = [];
-input.shift();
-input = input.map((x) => x.split(' '));
+input = input.split("\n");
+const initString = input.shift();
+const n = +input.shift();
+input = input.map((command) => command.split(" "));
 
-for (const value of input) {
-  if (value[0] === 'L' && leftStack.length !== 0) {
-    rightStack.push(leftStack.pop());
-  } else if (value[0] === 'D' && rightStack.length !== 0) {
-    leftStack.push(rightStack.pop());
-  } else if (value[0] === 'B') {
-    leftStack.pop();
-  } else if (value[0] === 'P') {
-    leftStack.push(value[1]);
+function solution(input, init) {
+  const leftStack = [...init.split("")];
+  const rightStack = [];
+
+  for (let command of input) {
+    if (command[0] === "L" && leftStack.length !== 0) {
+      rightStack.push(leftStack.pop());
+    }
+
+    if (command[0] === "D" && rightStack.length !== 0) {
+      leftStack.push(rightStack.pop());
+    }
+
+    if (command[0] === "B") {
+      leftStack.pop();
+    }
+
+    if (command[0] === "P") {
+      leftStack.push(command[1]);
+    }
   }
+
+  return leftStack.concat(rightStack.reverse()).join("");
 }
 
-const result = leftStack.join('') + rightStack.reverse().join('');
-console.log(result);
+console.log(solution(input, initString));

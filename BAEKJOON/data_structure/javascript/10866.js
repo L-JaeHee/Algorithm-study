291eba7a6f833b4013950844e89cb71d03e18224
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2023-02-22 15:59:28 +0900

    23.02.22 / 복습 및 스타일 변경

--- BAEKJOON/data_structure/javascript/10866.js
+++ BAEKJOON/data_structure/javascript/10866.js
@@ -1,72 +1,117 @@
-/*
-  10866: 덱/실버 4
-*/
-const fs = require('fs');
-const localPath = __dirname + '/input.txt';  // 로컬 실행용 경로
-const baekPath = '/dev/stdin';  // 백준 제출용 경로
-let input = fs.readFileSync(localPath).toString().trim().split('\n');
-input.shift();
+const fs = require("fs");
+let input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : fs
+        .readFileSync(__dirname + "/input.txt")
+        .toString()
+        .trim();
 
-const commands = {
-  push_back: function(deque, x) {
-    deque.push(x);
-  },
-  push_front: function(deque, x) {
-    deque.unshift(x);
-  },
-  pop_front: function(deque) {
-    if (deque.length === 0) {
-      return -1
-    } else {
-      return deque.shift();
+input = input.split("\n");
+const n = +input.shift();
+input = input.map((command) => command.split(" "));
+
+class Deque {
+  constructor() {
+    this.current = [];
+  }
+
+  pushFront(value) {
+    this.current.unshift(value);
+  }
+
+  pushBack(value) {
+    this.current.push(value);
+  }
+
+  popFront() {
+    if (this.current.length === 0) {
+      return -1;
     }
-  },
-  pop_back: function(deque) {
-    if (deque.length === 0) {
-      return -1
-    } else {
-      return deque.splice(deque.length - 1, 1)[0];
+
+    return this.current.shift();
+  }
+
+  popBack() {
+    if (this.current.length === 0) {
+      return -1;
     }
-  },
-  size: function(deque) {
-    return deque.length;
-  },
-  empty: function(deque) {
-    if (deque.length === 0) {
+
+    return this.current.pop();
+  }
+
+  size() {
+    return this.current.length;
+  }
+
+  empty() {
+    if (this.current.length === 0) {
       return 1;
-    } else {
-      return 0;
     }
-  },
-  front: function(deque) {
-    if (deque.length === 0) {
+
+    return 0;
+  }
+
+  front() {
+    if (this.current.length === 0) {
       return -1;
-    } else {
-      return deque[0];
     }
-  },
-  back: function(deque) {
-    if (deque.length === 0) {
+
+    return this.current[0];
+  }
+
+  back() {
+    if (this.current.length === 0) {
       return -1;
-    } else {
-      return deque[deque.length - 1];
     }
+
+    return this.current[this.current.length - 1];
   }
 }
 
-const deque = [];
-const result = [];
+function solution(input) {
+  const result = [];
+  const deque = new Deque();
 
-for (let value of input) {
-  let command = value.split(' ');
-  if (command[0] === 'push_back') commands.push_back(deque, command[1]);
-  else if (command[0] === 'push_front') commands.push_front(deque, command[1]);
-  else if (command[0] === 'pop_front') result.push(commands.pop_front(deque));
-  else if (command[0] === 'pop_back') result.push(commands.pop_back(deque));
-  else if (command[0] === 'size') result.push(commands.size(deque));
-  else if (command[0] === 'empty') result.push(commands.empty(deque));
-  else if (command[0] === 'front') result.push(commands.front(deque));
-  else if (command[0] === 'back') result.push(commands.back(deque));
+  for (let command of input) {
+    const [order, value] = command;
+
+    switch (order) {
+      case "push_front":
+        deque.pushFront(value);
+        break;
+
+      case "push_back":
+        deque.pushBack(value);
+        break;
+
+      case "pop_front":
+        result.push(deque.popFront());
+        break;
+
+      case "pop_back":
+        result.push(deque.popBack());
+        break;
+
+      case "size":
+        result.push(deque.size());
+        break;
+
+      case "empty":
+        result.push(deque.empty());
+        break;
+
+      case "front":
+        result.push(deque.front());
+        break;
+
+      case "back":
+        result.push(deque.back());
+        break;
+    }
+  }
+
+  return result.join("\n");
 }
 
-console.log(result.join('\n'));
+console.log(solution(input));

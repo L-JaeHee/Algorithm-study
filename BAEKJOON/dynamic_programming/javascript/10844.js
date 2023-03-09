f7640fee560d1040bc3f74794fc5c3fa896d0408
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2023-03-10 00:45:24 +0900

    23.03.10 / 복습 및 코드개선

--- BAEKJOON/dynamic_programming/javascript/10844.js
+++ BAEKJOON/dynamic_programming/javascript/10844.js
@@ -1,31 +1,26 @@
-/*
-  10844: 쉬운 계단 수/실버 1
-*/
-const fs = require('fs');
-const localPath = __dirname + '/input.txt';  // 로컬 실행용 경로
-const baekPath = '/dev/stdin';  // 백준 제출용 경로
-let input = +fs.readFileSync(localPath).toString().trim();
+const fs = require("fs");
+let input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : fs
+        .readFileSync(__dirname + "/input.txt")
+        .toString()
+        .trim();
 
-const dp = [-1, new Array(10).fill(0).map((x, idx) => {
-  if (idx > 0) return 1;
-  else return 0;
-})]
+function solution(input) {
+  const dp = [-1, [0, ...new Array(9).fill(1)]];
 
-for (let i = 2; i <= input; i++) {
-  let temp = new Array(10).fill(0);
-  for (let j = 0; j <= 9; j++) {
-    if (j === 0) {
-      temp[1] += dp[i - 1][0] % 1000000000;
-      continue;
+  for (let i = 2; i <= Number(input); i++) {
+    dp[i] = new Array(10).fill(0);
+    dp[i][0] = dp[i - 1][1];
+    dp[i][9] = dp[i - 1][8];
+
+    for (let j = 1; j < 9; j++) {
+      dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
     }
-    if (j === 9) {
-      temp[8] += dp[i - 1][9] % 1000000000;
-      continue;
-    }
-    temp[j - 1] = (temp[j - 1] + dp[i - 1][j]) % 1000000000;
-    temp[j + 1] = (temp[j + 1] + dp[i - 1][j]) % 1000000000;
   }
-  dp.push(temp);
+
+  return dp[input].reduce((sum, cur) => sum + cur) % 1000000000;
 }
 
-console.log(dp[input].reduce((sum, x) => sum + x) % 1000000000);
+console.log(solution(input));

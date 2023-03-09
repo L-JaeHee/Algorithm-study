1b119910a6f0301051ce9b37bda181e263750104
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2023-03-09 23:04:37 +0900

    23.03.09 / 복습 및 코드개선

--- BAEKJOON/dynamic_programming/javascript/15990.js
+++ BAEKJOON/dynamic_programming/javascript/15990.js
@@ -1,24 +1,33 @@
-/*
-  15990: 1,2,3 더하기 5/실버 2
-*/
-const fs = require('fs');
-const localPath = __dirname + '/input.txt';  // 로컬 실행용 경로
-const baekPath = '/dev/stdin';  // 백준 제출용 경로
-let input = fs.readFileSync(localPath).toString().trim().split('\n').map((x) => +x);
-input.shift();
+const fs = require("fs");
+let input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : fs
+        .readFileSync(__dirname + "/input.txt")
+        .toString()
+        .trim();
 
-const dp = [0, [1, 0, 0], [0, 1, 0], [1, 1, 1]];
+function solution(input) {
+  let [n, ...nums] = input.split("\n");
+  nums = nums.map((x) => Number(x));
+  const max = Math.max(...nums);
+  const result = [];
 
-for (let i = 4; i <= Math.max(...input); i++) {
-  let first = (dp[i - 1][1] + dp[i - 1][2]) % 1000000009;
-  let second = (dp[i - 2][0] + dp[i - 2][2]) % 1000000009;
-  let third = (dp[i - 3][0] + dp[i - 3][1]) % 1000000009;
-  dp.push([first, second, third]);
+  const dp = new Array(max + 1).fill(0);
+  dp[1] = [1, 0, 0];
+  dp[2] = [0, 1, 0];
+  dp[3] = [1, 1, 1];
+
+  for (let i = 4; i <= max; i++) {
+    dp[i] = [(dp[i - 1][1] + dp[i - 1][2]) % 1000000009, (dp[i - 2][0] + dp[i - 2][2]) % 1000000009, (dp[i - 3][0] + dp[i - 3][1]) % 1000000009];
+  }
+
+  for (let num of nums) {
+    const sum = dp[num].reduce((acc, cur) => acc + cur, 0) % 1000000009;
+    result.push(sum);
+  }
+
+  return result.join("\n");
 }
 
-const result = []
-for (let command of input) {
-  result.push((dp[command].reduce((sum, x) => sum + x)) % 1000000009);
-}
-
-console.log(result.join('\n'));
+console.log(solution(input));

c92f76b8ab1499d5adc2d4f073ffbfa3441d4107
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2023-03-19 00:04:07 +0900

    23.03.19 / 복습

--- BAEKJOON/dynamic_programming/javascript/2156.js
+++ BAEKJOON/dynamic_programming/javascript/2156.js
@@ -1,24 +1,23 @@
-/*
-  2156: 포도주 시식/실버 1
-*/
-const fs = require('fs');
-const localPath = __dirname + '/input.txt';  // 로컬 실행용 경로
-const baekPath = '/dev/stdin';  // 백준 제출용 경로
-let input = fs.readFileSync(localPath).toString().trim().split('\n');
+const fs = require("fs");
+let input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : fs
+        .readFileSync(__dirname + "/input.txt")
+        .toString()
+        .trim();
 
-const N = +input.shift();
-input = input.map(Number);
-input.unshift(0);
+function solution(input) {
+  input = input.split("\n").map((x) => Number(x));
+  const n = input.shift();
+  const dp = new Array(n + 3).fill(0);
+  input = [0, 0, 0].concat(input);
 
-const dp = [0, input[1], input[1] + input[2]];
-let temp;
+  for (let i = 3; i < n + 3; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + input[i], dp[i - 3] + input[i - 1] + input[i]);
+  }
 
-for (let i = 3; i <= N; i++) {
-  temp = [];
-  temp.push(dp[i - 1]);
-  temp.push(dp[i - 2] + input[i]);
-  temp.push(dp[i - 3] + input[i - 1] + input[i]);
-  dp.push(Math.max(...temp));
+  return dp[dp.length - 1];
 }
 
-console.log(dp[N]);
+console.log(solution(input));

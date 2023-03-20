6b493beb8077b8bf1739627cd6f265840a71e258
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2023-03-20 23:34:17 +0900

    23.03.20 / 복습

--- BAEKJOON/dynamic_programming/javascript/11055.js
+++ BAEKJOON/dynamic_programming/javascript/11055.js
@@ -1,21 +1,26 @@
-/*
-  11055: 가장 큰 증가 부분 수열/실버 2
-*/
-const fs = require('fs');
-const localPath = __dirname + '/input.txt';  // 로컬 실행용 경로
-const baekPath = '/dev/stdin';  // 백준 제출용 경로
-let input = fs.readFileSync(localPath).toString().trim().split('\n');
+const fs = require("fs");
+let input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : fs
+        .readFileSync(__dirname + "/input.txt")
+        .toString()
+        .trim();
 
-const N = +input.shift();
-input = input[0].split(' ').map(Number);
-const dp = [...input];
+function solution(input) {
+  let [n, nums] = input.split("\n");
+  nums = nums.split(" ").map(Number);
+  const dp = [...nums];
 
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < i; j++) {
-    if (input[i] > input[j]) {
-      dp[i] = Math.max(dp[i], dp[j] + input[i]);
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + nums[i]);
+      }
     }
   }
+
+  return Math.max(...dp);
 }
 
-console.log(Math.max(...dp));
+console.log(solution(input));

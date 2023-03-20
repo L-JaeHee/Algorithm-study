72da2adccad35009c609aa35326e6fde4ea9ee06
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2023-03-20 22:51:55 +0900

    23.03.20 / 복습

--- BAEKJOON/dynamic_programming/javascript/1932.js
+++ BAEKJOON/dynamic_programming/javascript/1932.js
@@ -1,19 +1,28 @@
-/*
-  1932: 정수 삼각형/실버 1
-*/
-const fs = require('fs');
-const localPath = __dirname + '/input.txt';  // 로컬 실행용 경로
-const baekPath = '/dev/stdin';  // 백준 제출용 경로
-let input = fs.readFileSync(localPath).toString().trim().split('\n');
+const fs = require("fs");
+let input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : fs
+        .readFileSync(__dirname + "/input.txt")
+        .toString()
+        .trim();
 
-const N = +input.shift();
-input = input.map((x) => x.split(' ').map(Number));
-input.unshift(0);
+function solution(input) {
+  let [n, ...triangle] = input.split("\n");
+  triangle = triangle.map((x) => x.split(" ").map((x) => Number(x)));
+  const dp = new Array(Number(n)).fill(0).map((_, idx) => new Array(idx + 1).fill(0));
+  dp[0][0] = triangle[0][0];
 
-for (let i = N - 1; i >= 1; i--) {
-  for (let j = 0; j < input[i].length; j++) {
-    input[i][j] += Math.max(input[i + 1][j], input[i + 1][j + 1]);
+  for (let i = 1; i < n; i++) {
+    dp[i][0] = dp[i - 1][0] + triangle[i][0];
+    dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+
+    for (let j = 1; j < i; j++) {
+      dp[i][j] = Math.max(dp[i - 1][j - 1] + triangle[i][j], dp[i - 1][j] + triangle[i][j]);
+    }
   }
+
+  return Math.max(...dp[n - 1]);
 }
 
-console.log(input[1][0]);
+console.log(solution(input));

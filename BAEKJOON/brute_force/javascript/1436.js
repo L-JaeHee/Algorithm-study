590eb7aebe2253aa1c6208ad630ddb92c9c8b845
JaeHeeLee <97137391+L-JaeHee@users.noreply.github.com>
2023-03-28 18:07:26 +0900

    23.03.28 / 복습

--- BAEKJOON/brute_force/javascript/1436.js
+++ BAEKJOON/brute_force/javascript/1436.js
@@ -1,23 +1,25 @@
-/*
-  1436: 영화감독 숌/실버 5
-*/
-const fs = require('fs');
-const localPath = __dirname + '/input.txt';  // 로컬 실행용 경로
-const baekPath = '/dev/stdin';  // 백준 제출용 경로
-let input = +fs.readFileSync(localPath).toString().trim();
+const fs = require("fs");
+let input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : fs
+        .readFileSync(__dirname + "/input.txt")
+        .toString()
+        .trim();
 
-let cnt = 1;
-let result = 666;
-let i = 667;
-const regex = /6{3}/;
+function solution(input) {
+  let n = 1;
+  let i = 666;
 
-while (cnt < input) {
-  let currentNumber = String(i);
-  if (regex.test(currentNumber)) {
-    cnt++;
-    result = currentNumber;
+  while (n < Number(input)) {
+    i++;
+
+    if (i.toString().match(/666/)) {
+      n++;
+    }
   }
-  i++;
+
+  return i;
 }
 
-console.log(result);
+console.log(solution(input));
